610e82502d498e0f296a1ba0bf8ce51f2218081c
FAVOUR <truelyfavoured@gmail.com>
2019-12-20 05:49:54 +0100

    Initial commit

---
+++ app/src/main/java/com/favour/fetchdata/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.favour.fetchdata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
