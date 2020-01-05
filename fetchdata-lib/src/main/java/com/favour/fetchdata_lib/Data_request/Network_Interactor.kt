dd321b18f63f0cf88c8d9fb4a4f2448c75ed1507
FAVOUR <truelyfavoured@gmail.com>
2020-01-05 01:53:50 +0100

    Partially Implimented  the response from the server

--- fetchdata-lib/src/main/java/com/favour/fetchdata_lib/Data_request/Network_Interactor.kt
+++ fetchdata-lib/src/main/java/com/favour/fetchdata_lib/Data_request/Network_Interactor.kt
@@ -1,10 +1,40 @@
 package com.favour.fetchdata_lib.Data_request
 
 import com.favour.fetchdata_lib.setup.RequestCreator
+import com.favour.fetchdata_lib.utils.checkNotNull
+import okhttp3.Call
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
 
 class Network_Interactor : RequestHandler() {
     override fun load(requestCreator: RequestCreator, callback: Callback) {
 
+        var request = createRequest(requestCreator)
+        val req = OkHttpClient()
+
+        req.newCall(request).enqueue(object : okhttp3.Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                callback.onError(e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+//               callback.onSuccess(response.body)
+            }
+        })
+    }
+
+    fun createRequest(requestCreator: RequestCreator): Request {
+
+        val uri = checkNotNull(requestCreator.uri, "Uri is empty")
+
+
+        val _request = Request.Builder()
+            .url(uri.toString())
+            .build()
+
+        return _request
     }
 
 
