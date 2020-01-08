8b0d8ed5244d757fdcc009819e639c03e6269d00
FAVOUR <truelyfavoured@gmail.com>
2020-01-08 07:01:13 +0100

    Setup method to decode bitmap pre-android P and for Android P

--- fetchdata-lib/src/main/java/com/favour/fetchdata_lib/utils/BitMapUtils.kt
+++ fetchdata-lib/src/main/java/com/favour/fetchdata_lib/utils/BitMapUtils.kt
@@ -1,12 +1,30 @@
 package com.favour.fetchdata_lib.utils
 
+import android.annotation.SuppressLint
 import android.graphics.Bitmap
+import androidx.annotation.RequiresApi
 import java.io.IOException
 
 class BitMapUtils {
 
     @Throws(IOException::class)
     fun decodeStream(): Bitmap {
+
+    }
+
+
+    companion object {
+
+        @RequiresApi(28)
+        @SuppressLint("Override")
+        private fun decodeStreamP(): Bitmap {
+
+        }
+
+        private decodeStreamPreP():Bitmap
+        {
+
+        }
 
     }
 }
